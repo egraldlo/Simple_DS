f208057fd8b1f327afe5ab71aca9b3c49c0b2599
egraldlo <egraldlo@gmail.com>
2015-05-02 16:14:00 +0800

    This project is for the simple data structure I will complete usually。

---
+++ src/Simple_DS.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Simple_DS.cpp
+// Author      : zhanglei
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!This project is for the simple data structure I will complete usually!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
